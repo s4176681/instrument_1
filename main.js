aa821eaf6bcfff79e079d26c3c3018945ede642b
Daniel Wong <s4176681@student.rmit.edu.au>
2026-08-13 10:11:38 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.body.style.backgroundColor = "red";
+//document.body.style.backgroundColor = "red";
+// get button
+const testButton = document.getElementById("test-button");
+
+// create instrument and connect to audio
+const synth = new Tone.Synth().toDestination();
+
+// action
+testButton.addEventListener("click", playTestNote);
+
+function playTestNote(){
+    synth.triggerAttackRelease("C4", "8n");
+}
