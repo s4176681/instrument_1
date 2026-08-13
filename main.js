9b46812561638e3a9934eb512d139fe05e24fcac
Daniel Wong <s4176681@student.rmit.edu.au>
2026-08-13 11:47:03 +1000

    Summary
    CSS colour changes
    created a standard modal
    ok button

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
+
 //document.body.style.backgroundColor = "red";
 // get button
 const testButton = document.getElementById("test-button");
 
+
+
+//MODAL
+//find intro modal
+const introModal = document.getElementById("intro-modal");
+//console.log(introModal); this actually slows it down
+introModal.showModal();
+//close
+const ok = document.getElementById("modal-close");
+ok.addEventListener("click", () => {
+    introModal.close();
+});
+//function closeIntroModal()
+introModal.addEventListener("close", toneInit);
+
+
+//TONE
 // create instrument and connect to audio
-const synth = new Tone.Synth().toDestination();
+// html -> js -> open modal -> ok -> modal closes -> audio init
+const synth = new Tone.Synth();
+
+function toneInit(){
+    synth.connect(Tone.Destination);
+}
 
 // action
 testButton.addEventListener("click", playTestNote);
@@ -11,3 +34,4 @@ testButton.addEventListener("click", playTestNote);
 function playTestNote(){
     synth.triggerAttackRelease("C4", "8n");
 }
+
