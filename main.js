5b048f0a59d714917e4ddbf3c323fb2ac11c7cd1
Daniel Wong <s4176681@student.rmit.edu.au>
2026-08-20 11:29:29 +1000

    Updates
    creating keys and more sound varieties

--- main.js
+++ main.js
@@ -2,21 +2,37 @@
 //document.body.style.backgroundColor = "red";
 // get button
 const testButton = document.getElementById("test-button");
-
-
-
+//
+const key = document.getElementById("key-test")
 //MODAL
 //find intro modal
 const introModal = document.getElementById("intro-modal");
 //console.log(introModal); this actually slows it down
-introModal.showModal();
 //close
-const ok = document.getElementById("modal-close");
-ok.addEventListener("click", () => {
+const okButton = document.getElementById("intro-modal-close");
+// mouse button hold?
+let mouseButtonDown = false;
+// mouse being held variable
+window.addEventListener("mousedown", function(){ //new
+    mouseButtonDown = true;
+});
+window.addEventListener("mouseup", function(){ //new
+    mouseButtonDown = false;
+});
+
+
+//MODAL
+introModal.showModal();
+// when click close
+okButton.addEventListener("click", function closeIntroModal() {
     introModal.close();
 });
 //function closeIntroModal()
 introModal.addEventListener("close", toneInit);
+
+
+
+
 
 
 //TONE
@@ -25,13 +41,22 @@ introModal.addEventListener("close", toneInit);
 const synth = new Tone.Synth();
 
 function toneInit(){
+    // connect synth
     synth.connect(Tone.Destination);
 }
 
 // action
-testButton.addEventListener("click", playTestNote);
+//testButton.addEventListener("click", playTestNote);
 
-function playTestNote(){
-    synth.triggerAttackRelease("C4", "8n");
+function playNote(e){
+    // find element
+    let keyPressed = e.target;
+    console.log(keyPressed);
+    // find data note
+    let note = keyPressed.dataset.note;
+    console.log(note);
 }
+
+testButton.addEventListener("mousedown", playNote);
+key.addEventListener("mousedown", playNote);
 
