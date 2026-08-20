a42f01d5c2b1d8a68dce974a04b693b2729ac1be
Daniel Wong <s4176681@student.rmit.edu.au>
2026-08-20 11:41:28 +1000

    Finished lesson

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ introModal.addEventListener("close", toneInit);
 //TONE
 // create instrument and connect to audio
 // html -> js -> open modal -> ok -> modal closes -> audio init
-const synth = new Tone.Synth();
+const synth = new Tone.PolySynth();
 
 function toneInit(){
     // connect synth
@@ -49,14 +49,36 @@ function toneInit(){
 //testButton.addEventListener("click", playTestNote);
 
 function playNote(e){
+    // find the element that the event ran on
+    let keyPressed = e.target;
+    console.log(keyPressed);
+    // find the data-note attribute of that element
+    let note = keyPressed.dataset.note;
+    console.log(note);
+    // play the note for the right amount of time
+    // if mouse button is held previously play note
+    if(mouseButtonDown === true){
+        synth.triggerAttack(note);
+    }
+}
+
+function endNote(e) {
     // find element
     let keyPressed = e.target;
     console.log(keyPressed);
     // find data note
     let note = keyPressed.dataset.note;
     console.log(note);
+    //right amount of time
+    synth.triggerRelease(note);
 }
 
 testButton.addEventListener("mousedown", playNote);
-key.addEventListener("mousedown", playNote);
+testButton.addEventListener("mouseenter", playNote);
+testButton.addEventListener("mouseup", endNote);
+testButton.addEventListener("mouseleave", endNote);
 
+key.addEventListener("mousedown", playNote);
+key.addEventListener("mouseenter", playNote);
+key.addEventListener("mouseup", endNote);
+key.addEventListener("mouseleave", endNote);
