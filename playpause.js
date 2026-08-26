42245b558b1314dccf936c151d5629ec4835df4d
Daniel Wong <s4176681@student.rmit.edu.au>
2026-08-27 09:30:18 +1000

    Created new JS
    For play pause button
    created a simple if else function
    linked to main index.html file

---
+++ playpause.js
@@ -0,0 +1,21 @@
+const playPauseButton = document.querySelector(".playPauseButton");
+const playPauseIcon = plaPauseButton.querySelector("img");
+
+let isPlaying = false;
+
+const playIconSrc = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+const pauseIconSrc = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+// defining images
+
+playPauseButton.addEventListener("click", function(){
+    isPlaying = !isPlaying; // ! = not
+    // flipping the boolean, toggle pattern for playpause button.
+
+    if(isPlaying){
+        playPauseIcon.src = pauseIconSrc;
+        playPauseIcon.alt = "Pause Button";
+    } else {
+        playPauseIcon.src = playIconSrc;
+        playPauseIcon.alt = "Play Button";
+    }
+});
