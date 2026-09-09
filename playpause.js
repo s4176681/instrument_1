278cd2f93992eb5f99b251dd008b3e3da0e3eee2
Daniel Wong <s4176681@student.rmit.edu.au>
2026-09-09 14:05:06 +1000

    Audio removed for now

--- playpause.js
+++ playpause.js
@@ -8,29 +8,18 @@ const pauseIconSrc = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
 // defining images
 
 
-// audio file playback
-const playbackButton = document.getElementById("playPauseButton");
-const audioTrack = document.getElementById("audio-track");
-
-
-
 playPauseButton.addEventListener("click", function(){
     isPlaying = !isPlaying; // ! = not
     // flipping the boolean, toggle pattern for playpause button.
 
     if(isPlaying){
-        audioTrack.play();
         playPauseIcon.src = pauseIconSrc;
         playPauseIcon.alt = "Pause Button";
         
         
     } else {
-        audioTrack.pause();
-        //audioTrack.paused === true;
         playPauseIcon.src = playIconSrc;
         playPauseIcon.alt = "Play Button";
         
     }
 });
-
-playPauseButton.addEventListener("click", playAudio);
