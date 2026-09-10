e8c4af85d951505b94353a5869997464184fa669
Daniel Wong <s4176681@student.rmit.edu.au>
2026-09-10 10:52:49 +1000

    COlour stays for main branch

--- main.js
+++ main.js
@@ -36,6 +36,28 @@ introModal.addEventListener("close", toneInit);
 
 
 
+// COLOURING
+const canvas = document.getElementById("visualiser");
+const ctx = canvas.getContext("2d");
+
+function flashCanvas(colour){ //colouring
+    ctx.fillStyle = colour;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+const noteColours = {
+    "C4": "lightpink",
+    "D4": "lightyellow",
+    "E4": "lightgreen",
+    "F4": "lightblue",
+    "G4": "plum",
+    "A4": "peachpuff",
+    "B4": "lightcyan",
+    "C5": "lightgray"
+}
+
+
+
 
 //TONE + usage
 // create instrument and connect to audio
@@ -64,7 +86,7 @@ function playNote(e){
     // if mouse button is held previously play note
     if(e.buttons === 1){ //// WORKS FOR OUR BROWSER, awkwardly optimised.
         synth.triggerAttack(note);
-
+        flashCanvas(noteColours[note]); // choose colour here
     }
 }
 
@@ -77,7 +99,7 @@ function endNote(e) {
     console.log(note);
     //right amount of time
     synth.triggerRelease(note);
-
+    flashCanvas("#ffffff76"); //reset colour
 }
 
 
